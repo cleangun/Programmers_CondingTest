3c5f0df1f29a000a51e313d3c247d3b15ad98118
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2024-04-24 10:50:55 +0900

    [Silver I] Title: 회의실 배정, Time: 280 ms, Memory: 59068 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,13 +1,12 @@
+import sys; input = sys.stdin.readline
 n = int(input())
 
 meet_lst = []
 
 for _ in range(n):
-    s,e = map(int,input().split())
-    meet_lst.append([s,e])
+    meet_lst.append(list(map(int,input().rstrip().split())))
 
 meet_lst.sort(key=lambda x : (x[1], x[0]))
-
 
 result = 0
 pe = 0 # prev_end
@@ -16,5 +15,4 @@ for s,e in meet_lst:
     if s >= pe:
         result += 1
         pe = e
-
 print(result)
