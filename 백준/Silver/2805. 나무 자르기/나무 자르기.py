d1e8d5a061f63e245936f4b440b64a7e6f62266e
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2024-04-30 17:04:29 +0900

    [Silver II] Title: 나무 자르기, Time: 1560 ms, Memory: 252004 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -5,29 +5,9 @@ n,m = map(int,input().rstrip().split())
 trees = [int(num) for num in input().rstrip().split()]
 trees = sorted(trees, key=lambda x: -x)
 
-# height = trees[0]
-# jump = 10 ** (len(str(height)) - 1)
-
 start = 0
 end = trees[0] + 1
 result = 0
-
-
-# while jump > 0:
-#     sum = 0
-#     for tree in trees:
-#         if tree <= height:
-#             break
-#         sum += tree - height
-#     # print(f"height = {height}")
-#     # print(f"sum = {sum}")
-#     if sum >= m:
-#         result = height
-#         height += jump
-#         jump = jump // 10
-#         continue
-#     else:
-#         height -= jump
 
 while start <= end:
     mid = (start + end) // 2
@@ -43,5 +23,4 @@ while start <= end:
         start = mid + 1
     else:
         end = mid - 1
-
 print(result)
