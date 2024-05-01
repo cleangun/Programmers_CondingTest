887253e0f6535f132047a2f163d88fb5d08be28e
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2024-05-01 12:19:18 +0900

    [Silver I] Title: IOIOI, Time: 372 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,30 +1,17 @@
-import sys; input = sys.stdin.readline
+N = int(input())
+M = int(input())
+S = input()
+answer, i, count = 0, 0, 0
 
-n = int(input())
-length = int(input())
+while i < (M - 1):
+    if S[i:i+3] == 'IOI':
+        i += 2
+        count += 1
+        if count == N:
+            answer += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
 
-line = input().rstrip()
-
-ch_i = n+1; ch_o = n
-result = 0
-
-for j in range(length - (n*2)):
-    ch = line[j]
-    
-    if ch == "I":
-        isOk = False
-        for i in range(1,ch_i+ch_o):
-            # i 여야함
-            if i % 2 == 0 and line[j+i] == "I":
-                isOk = True
-            # o 여야함    
-            elif i % 2 == 1 and line[j+i] == "O":
-                isOk = True
-            else:
-                isOk = False
-                break
-        if isOk:
-            result += 1
-print(result)
-            
-
+print(answer)
