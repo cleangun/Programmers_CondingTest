4e9c5040daa5aaa24062b34a51d285d5be0fdb2d
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2024-05-23 14:20:55 +0900

    [level 2] Title: 분기별 분화된 대장균의 개체 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
+++ 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
@@ -7,10 +7,5 @@ select case when extract(month from DIFFERENTIATION_DATE) between 1 and 3 then '
         end as QUARTER,
         count(*) as ECOLI_COUNT
   from ecoli_data
- group by case when extract(month from DIFFERENTIATION_DATE) between 1 and 3 then '1Q'
-               when extract(month from DIFFERENTIATION_DATE) between 4 and 6 then '2Q'
-               when extract(month from DIFFERENTIATION_DATE) between 7 and 9 then '3Q'
-               when extract(month from DIFFERENTIATION_DATE) between 10 and 12 then '4Q'
-           end;
-  
-  
+ group by 1
+ order by QUARTER;
